05208f696fbd8e4428c0381f1e173bce81d465a8
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-04-19 15:39:33 +0300

    a

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@ let leftImageElement = document.getElementById('img');
 let middleImageElement = document.getElementById('img1');
 let rightImageElement = document.getElementById('img2');
 let counts = 0;
-let maxAttempts = 25;
+let maxAttempts = 20;
 let leftIndex;
 let rightIndex;
 let middleIndex;
+let arrOfVotes = [];
+let arrOfShown = [];
+let arrOfnames = [];
+let arrOfisShown = [];
 BusMall.allImages = [];
 let table = document.getElementById('table');
+let container = document.getElementById('cont');
+
 
 new BusMall('bag', '../images/bag.jpg');
 new BusMall('banana', '../images/banana.jpg');
@@ -38,6 +44,8 @@ function BusMall(productName, imagePath) {
     this.imageShown = 0;
     this.votes = 0;
     BusMall.allImages.push(this);
+    arrOfnames.push(this.productName);
+
 };
 
 ///////////////////////////////////////////////////////////////////
@@ -53,57 +61,58 @@ function renderImages() {
     leftIndex = genrateRandomIndex();
     rightIndex = genrateRandomIndex();
     middleIndex = genrateRandomIndex();
-    while (leftIndex === rightIndex || leftIndex === middleIndex || rightIndex === middleIndex) {
+    while (leftIndex === rightIndex || leftIndex === middleIndex || rightIndex === middleIndex || arrOfShown.includes(leftIndex) || arrOfShown.includes(rightIndex) || arrOfShown.includes(middleIndex)) {
         leftIndex = genrateRandomIndex();
         rightIndex = genrateRandomIndex();
         middleIndex = genrateRandomIndex();
 
     }
-
+    arrOfShown = [];
     leftImageElement.src = BusMall.allImages[leftIndex].imagePath;
     BusMall.allImages[leftIndex].imageShown++;
-    x++;
+    arrOfShown.push(leftIndex);
     rightImageElement.src = BusMall.allImages[rightIndex].imagePath;
     BusMall.allImages[rightIndex].imageShown++;
-    x++;
+    arrOfShown.push(rightIndex);
     middleImageElement.src = BusMall.allImages[middleIndex].imagePath;
     BusMall.allImages[middleIndex].imageShown++;
-    x++;
+    arrOfShown.push(middleIndex);
 }
-
+console.log(arrOfShown);
 ///////////////////////////////////////////////////////////////////
 
 renderImages();
-leftImageElement.addEventListener('click', handleClicking);
-rightImageElement.addEventListener('click', handleClicking);
-middleImageElement.addEventListener('click', handleClicking);
+container.addEventListener('click', handleClicking);
+
 
 ///////////////////////////////////////////////////////////////////
 
 function handleClicking(event) {
 
-    counts++;
-    if (maxAttempts >= counts) {
-        if (event.target.id === 'img') {
-            BusMall.allImages[leftIndex].votes++;
-            y++;
-        } else if (event.target.id === 'img1') {
-            BusMall.allImages[rightIndex].votes++;
-            y++;
-        } else if (event.target.id === 'img2') {
-            BusMall.allImages[middleIndex].votes++;
-            y++;
-        }
-        if (counts != maxAttempts)
-            renderImages();
-        else {
-            tableRender();
-            leftImageElement.removeEventListener('click', handleClicking);
-            rightImageElement.removeEventListener('click', handleClicking);
-            middleImageElement.removeEventListener('click', handleClicking);
+    if (event.target.id === 'img' || event.target.id === 'img1' || event.target.id === 'img2') {
+        counts++;
+        if (maxAttempts >= counts) {
+            if (event.target.id === 'img') {
+                BusMall.allImages[leftIndex].votes++;
+                y++;
+            } else if (event.target.id === 'img1') {
+                BusMall.allImages[rightIndex].votes++;
+                y++;
+            } else if (event.target.id === 'img2') {
+                BusMall.allImages[middleIndex].votes++;
+                y++;
+            }
+            if (counts != maxAttempts)
+                renderImages();
+            else {
+                alert('you have finished');
+
+                container.removeEventListener('click', handleClicking);
+            }
 
         }
-
+    } else {
+        alert('you should click on the image');
     }
 }
 ///////////////////////////////////////////////////////////////////
@@ -117,13 +126,15 @@ function voteRender() {
     for (let i = 0; i < BusMall.allImages.length; i++) {
         let td = document.createElement('td');
         tr.appendChild(td);
-        td.textContent = BusMall.allImages[i].votes
+        td.textContent = BusMall.allImages[i].votes;
+        arrOfVotes.push(BusMall.allImages[i].votes);
+
     }
     imageShownRender()
 }
 
 ///////////////////////////////////////////////////////////////////
-function tableRender(){
+function tableRender() {
     let tr = document.createElement('tr');
     table.appendChild(tr);
     let th = document.createElement('th');
@@ -135,7 +146,6 @@ function tableRender(){
         tr.appendChild(td);
         td.textContent = BusMall.allImages[i].productName;
     }
-    voteRender();
 
 }
 ///////////////////////////////////////////////////////////////////
@@ -150,6 +160,47 @@ function imageShownRender() {
     for (let i = 0; i < BusMall.allImages.length; i++) {
         let td = document.createElement('td');
         tr.appendChild(td);
-        td.textContent = BusMall.allImages[i].imageShown
+        td.textContent = BusMall.allImages[i].imageShown;
+        arrOfShown.push(BusMall.allImages[i].imageShown);
+
+
+
     }
+}
+///////////////////////////////////////////////////////////////////
+function chart() {
+    let ctx = document.getElementById('myChart')
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: arrOfnames,
+            datasets: [{
+                label: 'NumberOfVotes',
+                data: arrOfVotes,
+                backgroundColor: [
+                    'rgb(0,0,0)',
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of Shown',
+                data: arrOfShown,
+                backgroundColor: [
+                    "rgb(200,50,50)"
+                ],
+                borderWidth: 1
+            }]
+        }
+    })
+}
+
+///////////////////////////////////////////////////////////////////
+
+let button = document.getElementById('btn');
+button.addEventListener('click', tableRender);
+
+function tableRender() {
+    voteRender();
+    chart();
+
+    button.removeEventListener('click', tableRender);
 }
